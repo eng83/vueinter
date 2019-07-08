518bd027d1dec3d76f19f81eb13f3191ce6ed214
BrownKook <kook@BrownKookui-MacBookPro.local>
2019-07-08 22:27:04 +0900

    3rd

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
         <ul>
-            <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+            <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
             <!-- <li v-for="todoItem in todoItems" v-bind:key="todoItems"> -->
+                <i class="fas fa-check" v-on:click="toggleComplete"></i>
                 {{todoItem}}
+                <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+                    <i class="fas fa-trash"></i>
+                </span>
             </li>
-            <li></li>
-            <li></li>
         </ul>
 
     </div>
@@ -20,7 +22,7 @@ export default {
         }
     },
     // 인스턴스가 생성되자마자 호출됨
-    created: function(){
+    created: function callwhencreated(){
         // console.log("Created");
         if(localStorage.length > 0){
             for(var i=0;i<localStorage.length;i++){
@@ -29,12 +31,53 @@ export default {
                 }
             }
         }
+    },
+    methods: {
+        removeTodo: function(todoItem, index){
+            //console.log(todoItem + " " +  index);
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index, 1);
+        },
+        toggleComplete: function(){
 
+        }
     }
 
 }
 </script>
 
-<style>
+<style scoped>
+ul{
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+}
+li{
+    display:flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 0.5rem 0;
+    margin: 0 0.9rem;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+}
+.checkBtn{
+    line-height: 45px;
+    color: #62acd6;
+    margin-right: 5px;
+}
+.checkBtnCompleted{
+    color: #b3adad;
 
+}
+.textCompleted{
+    text-decoration: line-through;
+    color: #b3adad;
+}
+.removeBtn{
+    margin-left: auto;
+    color: #de4343;
+}
 </style>
