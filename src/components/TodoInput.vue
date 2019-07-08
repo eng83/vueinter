a2afb4f0726d9cfae1329dcaac129d45dfb29973
BrownKook <kook@BrownKookui-MacBookPro.local>
2019-07-08 22:34:20 +0900

    4th

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -18,9 +18,13 @@ export default {
     },
     methods:{
         addTodo: function(){
-            // console.log(this.newTodoItem);
-            localStorage.setItem(this.newTodoItem,this.newTodoItem);
+            if(this.newTodoItem !== ""){
+                // console.log(this.newTodoItem);
+            var obj = {completed: false, item:this.newTodoItem};
+            //localStorage.setItem(this.newTodoItem, this.newTodoItem);
+            localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
             this.clearInput();
+            }
         },
         clearInput: function(){
             this.newTodoItem="";
